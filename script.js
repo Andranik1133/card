22f7c3e846a404f9e29664f64acea303f52d4a5d
Andranik1133 <ando.aleksanyan93@gmail.com>
2024-11-13 00:09:26 +0400

    render error

--- script.js
+++ script.js
@@ -3,12 +3,19 @@
 const btn = document.querySelector(".btn-country");
 const countriesContainer = document.querySelector(".countries");
 
+const renderError = function (msg) {
+  countriesContainer.insertAdjacentText("beforeend", msg);
+  //   countriesContainer.style.opacity = 1;
+};
+
 const renderCountry = function (data, className = "") {
   const html = `
     <article class="country ${className}">
       <img class="country__img" src="${data.flags.png}" />
       <div class="country__data">
-        <h3 class="country__name">${data.altSpellings[3] || data.name.common}</h3>
+        <h3 class="country__name">${
+          data.altSpellings[3] || data.name.common
+        }</h3>
         <h4 class="country__region">${data.region}</h4>
         <p class="country__row"><span>👫</span>${(
           +data.population / 1000000
@@ -23,25 +30,38 @@ const renderCountry = function (data, className = "") {
     </article>
     `;
   countriesContainer.insertAdjacentHTML("beforeend", html);
-  countriesContainer.style.opacity = 1;
+  //   countriesContainer.style.opacity = 1;
 };
 
 const getCountryData = function (country) {
   // country 1
   fetch(`https://restcountries.com/v3.1/name/${country}`)
-    .then((response) => response.json())
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`country not found ${response.status}`);
+      }
+      return response.json();
+    })
     .then((data) => {
-        console.log(data)
       renderCountry(data[0]);
       const neighbour = data[0].borders[0];
 
       if (!neighbour) return;
 
       //   country 2
-      return fetch(`https://restcountries.com/v3.1/alpha/${neighbour}`)
-        .then((response) => response.json())
-        .then((data) => renderCountry(data[0], "neighbour"));
+      return fetch(`https://restcountries.com/v3.1/alpha/${neighbour}`);
+    })
+    .then((response) => response.json())
+    .then((data) => renderCountry(data[0], "neighbour"))
+    .catch((err) => {
+      console.error(`${err} 💥💥💥💥💥 `);
+      renderError(`somthing went wrong 💥💥💥💥💥 ${err.message} try again`);
+    })
+    .finally(() => {
+      countriesContainer.style.opacity = 1;
     });
 };
 
-getCountryData("arm");
+btn.addEventListener("click", function () {
+  getCountryData("arm");
+});
